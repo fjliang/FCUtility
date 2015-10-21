6c63ad5777e82396ffea707547580eb963850a86
FJL <470987684@qq.com>
2015-10-21 14:05:16 +0800

    添加General文件

---
+++ FCUtility/General/FCUtility.h
@@ -0,0 +1,10 @@
+//
+// Created by 123456 on 15/10/21.
+// Copyright (c) 2015 fc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface FCUtility : NSObject
+@end
---
+++ FCUtility/General/FCUtility.m
@@ -0,0 +1,12 @@
+//
+// Created by 123456 on 15/10/21.
+// Copyright (c) 2015 fc. All rights reserved.
+//
+
+#import "FCUtility.h"
+
+
+@implementation FCUtility {
+
+}
+@end
